03100842c0bc7f7167ccccdfa0cc45f31895adad
ArthurWilbrod <comptwil@gmail.com>
2019-12-14 11:22:45 +0100

    aa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,11 +27,25 @@
 //= require js/plugins/jasny-bootstrap.min
 //= require js/plugins/nouislider.min
 //= require js/plugins/bootstrap-datetimepicker.min
+
 //= require jquery3
 //= require jquery_ujs
-
 //= require bootstrap
+//= require js/now-ui-kit
+//= require activestorage
+//= require_tree .
 
+//= require jquery3
+//= require jquery_ujs
+//= require assets/js/core/popper.min
+//= require bootstrap
+//= require assets/js/plugins/moment.min
+//= require assets/js/plugins/bootstrap-switch
+//= require assets/js/plugins/bootstrap-tagsinput
+//= require assets/js/plugins/bootstrap-selectpicker
+//= require assets/js/plugins/jasny-bootstrap.min
+//= require assets/js/plugins/nouislider.min
+//= require assets/js/plugins/bootstrap-datetimepicker.min
 //= require assets/js/now-ui-kit
 //= require activestorage
 //= require_tree .
